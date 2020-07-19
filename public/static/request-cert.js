24bf2f4cb14fafd06abd575866f1fe3de0dc5e35
ATOM | lis <atom.bitbucket@gmail.com>
2020-07-19 17:25:58 +0200

    Initial commit.

--- public/static/request-cert.js
+++ public/static/request-cert.js
@@ -1,2 +1,5 @@
-const cert = "https://demo.atom-lis.com/cert";
-const request = "https://demo.atom-lis.com/sign-message?request=";
+// const cert = "https://localhost:4001/cert";
+// const request = "https://localhost:4001//sign-message?request=";
+
+const cert = "https://biolab.atom-lis.com/cert";
+const request = "https://biolab.atom-lis.com/sign-message?request=";
