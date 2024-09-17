5778891af26c1a409d5906506d3c7e43733a39af
ATOM | Lab Software <atom.bitbucket@gmail.com>
2024-09-17 20:45:56 +0200

    17.09.2024

--- controllers/OdobravanjeController.js
+++ controllers/OdobravanjeController.js
@@ -1576,8 +1576,8 @@ odobravanjeController.SacuvajRezultate = function (req, res) {
               sample.tests.forEach((element) => {
                 req.body.rezultati.forEach((rez) => {
 
-                  console.log(rez.laIDE)  // - Ovo je undefined, iz toga razloga se uslov ne ispunjava
-
+                  console.log(rez)  // - Ovo je undefined, iz toga razloga se uslov ne ispunjava
+                  console.log(elementi.labassay)
                   // if (
                   //   element.labassay.equals(
                   //     mongoose.Types.ObjectId(rez.laIDE)
